fc4e0df26808b0a73ff84c022b26a47fc3adb16e
LogoArt <hanakuriart@gmai.com>
2025-06-22 22:24:09 +0900

    Works-Logo ページ作成 カードjs作成

---
+++ assets/js/card-generator.js
@@ -0,0 +1,282 @@
+// /assets/js/cards-generator.js
+const works = [
+  {
+    image: "assets/image/works-logo-kusunoki.webp",
+    title: "KUSUNOKI CAFE",
+    date: "2024-04-06",
+    link: "https://example.com/sunset-design"
+  },
+  {
+    image: "assets/image/works-logo-aki.webp",
+    title: "Aki City",
+    date: "2024-01-31",
+    link: "https://example.com/sunset-design"
+  },
+    {
+  image: "assets/image/works-logo-autdoor.webp",
+  title: "Autdoor Brand",
+  date: "2023-09-07",
+  link: "https://example.com/sunset-design"
+  },
+  {
+    image: "assets/image/works-logo-laitier.webp",
+    title: "Litier",
+    date: "2023-02-16",
+    link: "https://example.com/sunset-design"
+  },
+  {
+    image: "assets/image/works-logo-zeck.webp",
+    title: "ZECK",
+    date: "2024-08-22",
+    link: "https://example.com/sunset-design"
+  },
+  {
+    image: "assets/image/works-logo-rsaw.webp",
+    title: "Rising Arrow",
+    date: "2024-07-12",
+    link: "https://example.com/sunset-design"
+  },
+  {
+    image: "assets/image/works-logo-airy.webp",
+    title: "AIRY",
+    date: "2023-12-10",
+    link: "https://example.com/sunset-design"
+  },
+  {
+    image: "assets/image/works-logo-palxea.webp",
+    title: "Palxea",
+    date: "2023-08-08",
+    link: "https://example.com/sunset-design"
+  },
+  {
+    image: "assets/image/works-logo-trackrock.webp",
+    title: "Track Rock",
+    date: "2024-09-28",
+    link: "https://example.com/sunset-design"
+  },
+  {
+    image: "assets/image/works-logo-rht.webp",
+    title: "RIHITO",
+    date: "2024-04-15",
+    link: "https://example.com/sunset-design"
+  },
+    {
+    image: "assets/image/works-logo-planet.webp",
+    title: "Planet",
+    date: "2024-08-21",
+    link: "https://example.com/sunset-design"
+  },
+    {
+    image: "assets/image/works-logo-m10.webp",
+    title: "M10",
+    date: "2024-11-28",
+    link: "https://example.com/sunset-design"
+  },
+  {
+    image: "assets/image/works-logo-shiraga.webp",
+    title: "KAZUO SHIRAGA",
+    date: "2024-01-24",
+    link: "https://example.com/sunset-design"
+  },
+  {
+    image: "assets/image/works-logo-300.webp",
+    title: "300",
+    date: "2024-07-20",
+    link: "https://example.com/sunset-design"
+  },
+    {
+    image: "assets/image/works-logo-kinetic.webp",
+    title: "KINETIC",
+    date: "2024-01-21",
+    link: "https://example.com/sunset-design"
+  },
+  {
+    image: "assets/image/works-logo-gk.webp",
+    title: "GK",
+    date: "2023-10-28",
+    link: "https://example.com/sunset-design"
+  },
+    {
+    image: "assets/image/works-logo-abobo.webp",
+    title: "Abobo",
+    date: "2024-08-07",
+    link: "https://example.com/sunset-design"
+  },
+    {
+    image: "assets/image/works-logo-nai'a.webp",
+    title: "Ni'a Air plain",
+    date: "2024-04-23",
+    link: "https://dribbble.com/shots/25388253-Branding-for-Nai-a-Airlines"
+  },
+  {
+    image: "assets/image/works-logo-logo.webp",
+    title: "L・O・G・O",
+    date: "2023-06-29",
+    link: "https://example.com/sunset-design"
+  },
+  {
+    image: "assets/image/works-logo-sihouk.webp",
+    title: "Sihouk",
+    date: "2023-08-20",
+    link: "https://example.com/sunset-design"
+  },
+      {
+    image: "assets/image/works-logo-logoart.webp",
+    title: "LogoArt",
+    date: "2023-03-12",
+    link: "https://example.com/sunset-design"
+  },
+    {
+    image: "assets/image/works-logo-boat.webp",
+    title: "Boat",
+    date: "2023-04-01",
+    link: "https://example.com/sunset-design"
+  },
+  {
+    image: "assets/image/works-logo-in.webp",
+    title: "In",
+    date: "2024-08-28",
+    link: "https://example.com/sunset-design"
+  },
+    {
+    image: "assets/image/works-logo-h-line.webp",
+    title: "Logo H-line",
+    date: "2022-07-25",
+    link: "https://example.com/sunset-design"
+  },
+  {
+    image: "assets/image/works-logo-pen.webp",
+    title: "Pen",
+    date: "2023-04-02",
+    link: "https://example.com/sunset-design"
+  },
+  {
+    image: "assets/image/works-logo-romia.webp",
+    title: "Romia",
+    date: "2023-05-26",
+    link: "https://example.com/sunset-design"
+  },
+  {
+    image: "assets/image/works-logo-scratch.webp",
+    title: "Scratch Code",
+    date: "2022-11-10",
+    link: "https://example.com/sunset-design"
+  },
+  {
+    image: "assets/image/works-logo-k-line.webp",
+    title: "Logo K-line",
+    date: "2022-07-25",
+    link: "https://example.com/sunset-design"
+  },
+  {
+    image: "assets/image/works-logo-mountain.webp",
+    title: "Mountain",
+    date: "2022-06-19",
+    link: "https://example.com/sunset-design"
+  },
+  {
+    image: "assets/image/works-logo-fedex.webp",
+    title: "Fedex Remake",
+    date: "2023-04-16",
+    link: "https://example.com/sunset-design"
+  },
+  {
+    image: "assets/image/works-logo-fedex.webp",
+    title: "Fedex Remake",
+    date: "2023-04-16",
+    link: "https://example.com/sunset-design"
+  },
+    {
+    image: "assets/image/works-logo-xininty.webp",
+    title: "Xininty",
+    date: "2023-08-20",
+    link: "https://example.com/sunset-design"
+  },
+    {
+    image: "assets/image/works-logo-jep.webp",
+    title: "Jepcal",
+    date: "2023-08-20",
+    link: "https://example.com/sunset-design"
+  },
+    {
+    image: "assets/image/works-logo-r.webp",
+    title: "Logo R-bird",
+    date: "2023-08-20",
+    link: "https://example.com/sunset-design"
+  },
+    {
+    image: "assets/image/works-logo-area.webp",
+    title: "Area",
+    date: "2023-08-29",
+    link: "https://example.com/sunset-design"
+  },
+    {
+    image: "assets/image/works-logo-a-rose.webp",
+    title: " Logo A-rose",
+    date: "2023-08-29",
+    link: "https://example.com/sunset-design"
+  },
+    {
+    image: "assets/image/works-logo-sphere.webp",
+    title: "Sphere",
+    date: "2023-04-10",
+    link: "https://example.com/sunset-design"
+  },
+    {
+    image: "assets/image/works-logo-ts.webp",
+    title: "Logo T+S",
+    date: "2023-02-07",
+    link: "https://example.com/sunset-design"
+  },
+    {
+    image: "assets/image/works-logo-tiger.webp",
+    title: "Tiger",
+    date: "2023-01-07",
+    link: "https://example.com/sunset-design"
+  },
+    {
+    image: "assets/image/works-logo-w.webp",
+    title: "Logo W",
+    date: "2023-01-15",
+    link: "https://example.com/sunset-design"
+  },
+]
+
+// Generate work cards dynamically
+const container = document.getElementById("worksContainer");
+
+works.forEach(work => {
+  const card = document.createElement("div");
+  card.className = "work-card";
+
+  card.innerHTML = `
+  <a href="${work.link}" target="_blank" style="text-decoration: none; color: #000;">
+    <div class="work-card">
+      <div class="work-image">
+        <img src="${work.image}" alt="${work.title}">
+      </div>
+      <div class="work-divider"></div>
+      <div class="work-meta font-Acumin-default">
+        <p class="label font-Acumin-default">Title</p>
+        <p class="title">${work.title}</p>
+      </div>
+      <p class="work-date font-Acumin-default">${work.date}</p>
+    </div>
+  </a>
+`;
+
+  container.appendChild(card);
+});
+
+// タイトルの文字数に応じてletter-spacingを調整
+const titles = document.querySelectorAll('.work-meta .title');
+titles.forEach(title => {
+  const len = title.textContent.length;
+  if (len > 15) {
+    title.style.letterSpacing = "1px";
+  } else if (len > 10) {
+    title.style.letterSpacing = "1.5px";
+  } else {
+    title.style.letterSpacing = "2.87px";
+  }
+});
